3b04f3c3903d6d2b5f2c2630c1cdeee9c25ed169
Brian Helba <brian.helba@kitware.com>
2018-02-22 20:07:48 -0500

    Special start-up routing is no longer needed

--- web_external/main.js
+++ web_external/main.js
@@ -1,5 +1,4 @@
 import events from 'girder/events';
-import router from 'girder/router';
 import App from 'girder/views/App';
 
 // Import all stylesheets
@@ -16,16 +15,7 @@ $(() => {
     events.trigger('g:appload.before');
     const app = new App({
         el: 'body',
-        start: false,
         parentView: null
     });
-    app.start()
-        .done(() => {
-            // The default layout will always show on the first render (since app.navigateTo isn't
-            // called), so trigger a normal navigation to hide it again.
-            // TODO: This should use the current location, instead of the home page, to allow direct
-            // loading of URLs to work.
-            router.navigate('', {trigger: true});
-        });
     events.trigger('g:appload.after', app);
 });
