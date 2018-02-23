d33add895b0511667093a9e1f19aab9a82a41802
Brian Helba <brian.helba@kitware.com>
2018-02-22 19:58:53 -0500

    Add static analysis tests to web_external

--- plugin.cmake
+++ plugin.cmake
@@ -1,1 +1,12 @@
 add_standard_plugin_tests()
+
+# External client static analysis
+add_eslint_test(
+  tech_journal_external
+  "${CMAKE_CURRENT_LIST_DIR}/web_external")
+add_puglint_test(
+  tech_journal_external
+  "${CMAKE_CURRENT_LIST_DIR}/web_external")
+add_stylint_test(
+  tech_journal_external
+  "${CMAKE_CURRENT_LIST_DIR}/web_external")
