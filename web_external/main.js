a825c371bcd4ef239b429c1ac60cf5f60c233916
Roni Choudhury <roni.choudhury@kitware.com>
2018-03-21 11:27:54 -0400

    Place Google analytics bootstrapping into app entry point

--- web_external/main.js
+++ web_external/main.js
@@ -12,14 +12,27 @@ import './stylesheets/item.comments.styl';
 import './routes';
 
 $(() => {
+    const gaKey = process.env.GA_KEY;
+    if (gaKey) {
+      // Inject the Google Analytics framework.
+      let s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.src = `https://www.googletagmanager.com/gtag/js?id=${gaKey}`;
+      document.getElementsByTagName('head')[0].appendChild(s);
+
+      // Launch tracking for this site.
+      window.dataLayer = window.dataLayer || [];
+      const gtag = function () {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', gaKey);
+    }
+
     events.trigger('g:appload.before');
     const app = new App({
         el: 'body',
         parentView: null
     });
     events.trigger('g:appload.after', app);
-
-    if (process.env.GA_KEY) {
-      console.log(process.env.GA_KEY);
-    }
 });
