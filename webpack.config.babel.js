144a8ba2a0b824b0d4612031b97feab5dbf225b8
Roni Choudhury <roni.choudhury@kitware.com>
2018-04-13 15:21:39 -0400

    Get project to build independently

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,13 +4,13 @@ export default {
   entry: './web_external/main.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'js/[name].[chunkhash].js',
-    chunkFilename: 'js/[id].[chunkhash].js'
+    filename: 'js/[name].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -23,6 +23,22 @@ export default {
             ]
           }
         }
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.woff$|\.wav$|\.mp3$|\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$|\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader'
       }
     ]
   }
