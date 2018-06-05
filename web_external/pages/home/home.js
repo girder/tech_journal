569ffda1fc06e4a6b39c7f5049482bfe1378441c
Joseph Snyder <joe.snyder@kitware.com>
2018-06-05 11:37:14 -0400

    Add search building for each path
    Add the code which mirrors the OTJ code to build the query to send each
    time a new search event is triggered.
    
    Ensure that the search terms clear correctly when the clear button
    is pressed.
    
    Ensure that the "Show more results" link behaves as expected by correcting
    the startIndexes sent and checking for the length of the overall return.

--- web_external/pages/home/home.js
+++ web_external/pages/home/home.js
@@ -1,4 +1,5 @@
 import View from 'girder/views/View';
+import router from 'girder/router';
 import { restRequest } from 'girder/rest';
 
 import MenuBarView from '../../views/menuBar.js';
@@ -10,62 +11,38 @@ const HomePage = View.extend({
     events: {
         'click #search_button': function (event) {
             // search the available submissions for the text entered in the box
-            var searchText = $('#live_search').val();
-            restRequest({
-                type: 'GET',
-                path: `journal/${this.collectionID}/search?text=${searchText}`
-            }).done((resp) => {
-                this.render(resp, searchText, this.collectionID, 0);
-            });
+            this.buildSearch(this.collectionID, 0);
         },
         'click #clear_button': function (event) {
             // search the available submissions for the text entered in the box
-            this.initialize();
+            this.$('#live_search').val('');
+            this.$('.filterOption:checked').each(function (index, filter) {
+                filter.checked = false;
+            });
+            if (window.location.hash) {
+                router.navigate(``, {trigger: true});
+            } else {
+                this.buildSearch(this.collectionID, 0);
+            }
         },
         'click .issueTitle': function (event) {
             // search the available submissions for the text entered in the box
             $('.issueSelected').removeClass('issueSelected');
             $(event.currentTarget.parentNode).addClass('issueSelected');
             // Use the journal API to filter by selected submission
-            restRequest({
-                type: 'GET',
-                path: `journal/${this.defaultJournal}/submissions?strtIndex=0&filterID=${$(event.currentTarget.parentNode).attr('key')}`
-            }).done((jrnResp) => {
-                // Only update the search results, leaving the menu bar and selected issue intact.
-                this.$('.searchResults').html(IndexEntryViewTemplate({info: {'submissions': jrnResp}}));
-            });
+            this.buildSearch(this.collectionID, 0);
         },
         'click #showMoreResults': function (event) {
-            this.getSubmissions(this.collectionID, this.querystring, $('.SearchResultEntry').length);
+            this.buildSearch(this.collectionID, this.$('.SearchResultEntry').length);
         },
         'click .filterOption': function (event) {
-            var queryString = '';
-            if (this.$('.filterOption:checked').length) {
-                this.$('.categoryTree').each(function (index, category) {
-                    var catName = $(category).siblings('h4').text();
-                    if (queryString !== '') {
-                        queryString += ',';
-                    }
-                    queryString += `"${catName}":[`;
-                    var innerQuery = '';
-                    $(category).find('.filterOption:checked').each(function (index, filter) {
-                        if (innerQuery !== '') {
-                            innerQuery += ',';
-                        }
-                        innerQuery += `"${$(filter).attr('val')}"`;
-                    });
-                    queryString += `${innerQuery}]`;
-                });
-                this.querySubmissions(this.collectionID, queryString, 0);
-            } else {
-                this.initialize(this.querystring);
-            }
+            this.buildSearch(this.collectionID, 0);
         }
     },
     initialize: function (query) {
         restRequest({
             type: 'GET',
-            path: 'journal/setting',
+            url: 'journal/setting',
             data: {
                 list: JSON.stringify([
                     'tech_journal.default_journal'
@@ -78,33 +55,42 @@ const HomePage = View.extend({
             if (!$.isEmptyObject(query.collection)) {
                 this.collectionID = query.collection;
             }
-
-            if (!$.isEmptyObject(query.query)) {
-                this.querystring = query.query;
-                this.querySubmissions(this.collectionID, this.querystring, 0);
-            } else {
-                this.getSubmissions(this.collectionID, this.querystring, 0);
-            }
+            this.buildSearch(this.collectionID, 0);
         }); // End getting of OTJ Collection value setting
     },
     render: function (subData, searchVal, collection, startIndex) {
         var pendingSubs = 0;
         restRequest({
             type: 'GET',
-            path: `journal/${collection}/issues`
+            url: `journal/${collection}/issues`
         }).done((jrnResp) => {
             restRequest({
                 type: 'GET',
-                path: `journal/${this.defaultJournal}/pending?`
+                url: `journal/${this.defaultJournal}/pending?`
             }).done((pendRsp) => {
                 pendingSubs = pendRsp.length;
                 if (!this.$('.searchResults').length) {
                     this.$el.html(HomeTemplate({info: { 'issues': jrnResp }}));
                 }
+                // Check to see if we blank the existing submissions or not
                 if (startIndex === 0) {
                     this.$('.searchResults').html('');
                 }
-                this.$('.searchResults').html(this.$('.searchResults').html() + IndexEntryViewTemplate({info: {'submissions': subData}}));
+                var submissions = subData;
+                // Check to see if we need the "See more files" link
+                if (startIndex + 20 > subData.length) {
+                    this.$('#showMoreResults').hide();
+                } else {
+                    submissions = subData.slice(startIndex, startIndex + 20);
+                    this.$('#showMoreResults').show();
+                }
+                // Check to see if we have to add the "No results" message
+                if (subData.length === 0) {
+                    this.$('#noResultElement').show();
+                } else {
+                    this.$('#noResultElement').hide();
+                    this.$('.searchResults').html(this.$('.searchResults').html() + IndexEntryViewTemplate({info: {'submissions': submissions}}));
+                }
                 new MenuBarView({ // eslint-disable-line no-new
                     el: this.$el,
                     parentView: this,
@@ -114,7 +100,7 @@ const HomePage = View.extend({
                 if (this.$('#treeWrapper').find('.treeEntry').length < 3) {
                     restRequest({
                         type: 'GET',
-                        path: 'journal/categories?tag=categories'
+                        url: 'journal/categories?tag=categories'
                     }).done((resp) => {
                         for (var key in resp) {
                             this.$('#treeWrapper').html(this.$('#treeWrapper').html() + CategoryTemplate({'catName': resp[key]['key'], 'values': resp[key]['value']}));
@@ -126,23 +112,59 @@ const HomePage = View.extend({
         return this;
     },
 
+    buildSearch: function (collection, startIndex) {
+        /* Add all objects to the json file to query
+        *
+        *  targetIssue
+        *  text in live_search
+        *  selected filters
+        *
+        */
+        var queryString = '';
+        if (window.location.hash && !window.location.hash.includes('dialog')) {
+            queryString += decodeURI(window.location.hash.substring(8));
+        }
+
+        var searchText = $('#live_search').val();
+        if (searchText !== undefined && searchText !== '') {
+            if (queryString !== '') {
+                queryString += ',';
+            }
+            searchText = searchText.replace(/"/g, '&quot;');
+            queryString += `"text": "${searchText}"`;
+        }
+        var issueId = $('.issueSelected').attr('key');
+        if (issueId !== undefined && issueId !== '' && issueId !== '*') {
+            if (queryString !== '') {
+                queryString += ',';
+            }
+            queryString += `"issueId": "${issueId}"`;
+        }
+        if (this.$('.filterOption:checked').length) {
+            this.$('.categoryTree').each(function (index, category) {
+                var catName = $(category).siblings('h4').text();
+                if (queryString !== '') {
+                    queryString += ',';
+                }
+                queryString += `"${catName}":[`;
+                var innerQuery = '';
+                $(category).find('.filterOption:checked').each(function (index, filter) {
+                    if (innerQuery !== '') {
+                        innerQuery += ',';
+                    }
+                    innerQuery += '"' + $(filter).attr('val') + '"';
+                });
+                queryString += innerQuery + ']';
+            });
+        }
+        this.querySubmissions(this.collectionID, queryString, startIndex);
+    },
     querySubmissions: function (collection, queryString, startIndex) {
         restRequest({
             type: 'GET',
-            path: `journal/${collection}/search?query={${queryString}}`
+            url: `journal/${collection}/search?query={` + encodeURIComponent(queryString) + '}'
         }).done((jrnResp) => {
-            this.render(jrnResp, 'Search...', collection, startIndex);
-        });
-    },
-    getSubmissions: function (collection, queryString, startIndex) {
-        restRequest({
-            type: 'GET',
-            path: `journal/${collection}/submissions?strtIndex=${startIndex}&filterID=*`,
-            params: {
-                filterID: '*'
-            }
-        }).done((jrnResp) => {
-            this.render(jrnResp, 'Search...', collection, startIndex);
+            this.render(jrnResp, $('#live_search').val(), collection, startIndex);
         });
     }
 });
