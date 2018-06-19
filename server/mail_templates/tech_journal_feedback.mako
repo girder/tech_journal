1c4bb58dcf23eb32af5ccc2f62d5b3c37c76d2d3
Joseph Snyder <joe.snyder@kitware.com>
2018-06-19 16:47:38 -0400

    Add email for Feedback page
    Add a valid email template and public API  that allows feedback to be sent by
    any visitor to the Feedback page.

---
+++ server/mail_templates/tech_journal_feedback.mako
@@ -0,0 +1,10 @@
+<%include file="tech_journal_header.mako"/>
+A user has submitted feedback:
+
+<div style="font-size: 13px; color: #000000; margin-bottom: 12px;">
+<b>Reporter of the problem:</b> ${title} </br>
+<b>Problem Location:</b>${where}</br>
+<b>Problem Description:</b>${summary}</br>
+</div>
+
+<%include file="tech_journal_footer.mako"/>
--- web_external/views/feedback.js
+++ web_external/views/feedback.js
@@ -1,4 +1,6 @@
 import View from 'girder/views/View';
+import events from 'girder/events';
+import { restRequest } from 'girder/rest';
 
 import MenuBarView from './menuBar.js';
 import FeedbackViewTemplate from '../templates/journal_help_feedback.pug';
@@ -6,6 +8,28 @@ import FeedbackViewTemplate from '../templates/journal_help_feedback.pug';
 var FeedbackView = View.extend({
 
     events: {
+        'submit #form1': function (event) {
+            event.preventDefault();
+            var feedbackInfo = {};
+            feedbackInfo['title'] = this.$('#Title').val();
+            feedbackInfo['where'] = this.$('#Where').val();
+            feedbackInfo['summary'] = this.$('#Summary').val();
+
+            restRequest({
+                type: 'POST',
+                path: `journal/feedback`,
+                contentType: 'application/json',
+                data: JSON.stringify(feedbackInfo),
+                error: null
+            }).done((resp) => {
+                events.trigger('g:alert', {
+                    icon: 'ok',
+                    text: 'Email sent.',
+                    type: 'success',
+                    timeout: 4000
+                });
+            });
+        }
     },
     initialize: function () {
         this.render();
