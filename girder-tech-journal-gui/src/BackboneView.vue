e8ddcb6a3e9f2dd4bca5764f4ec32f330f47ca57
Brian Helba <brian.helba@kitware.com>
2019-01-25 02:49:51 -0500

    Add support for embedding Vue in Backbone and vice versa

---
+++ girder-tech-journal-gui/src/BackboneView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'BackboneView',
+  props: {
+    view: {
+      type: Function,
+      required: true,
+    },
+    settings: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    render: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      viewInstance: null,
+    };
+  },
+  mounted() {
+    this.viewInstance = new this.view({ // eslint-disable-line new-cap
+      el: this.$el,
+      parentView: null,
+      ...this.settings,
+    });
+    if (this.render) {
+      this.viewInstance.render();
+    }
+  },
+  beforeDestroy() {
+    if (this.viewInstance) {
+      this.viewInstance.destroy();
+    }
+  },
+};
+</script>
