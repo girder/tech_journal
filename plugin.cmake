f837c798396d50390703ce1ed79ff1d0fd4455db
Brian Helba <brian.helba@kitware.com>
2019-01-30 15:29:49 -0500

    Fix remaining front-end tests

--- plugin.cmake
+++ plugin.cmake
@@ -1,9 +1,6 @@
 add_standard_plugin_tests()
 
 # External client static analysis
-add_eslint_test(
-  tech_journal_external
-  "${CMAKE_CURRENT_LIST_DIR}/girder-tech-journal-gui/src")
 add_puglint_test(
   tech_journal_external
   "${CMAKE_CURRENT_LIST_DIR}/girder-tech-journal-gui/src")
