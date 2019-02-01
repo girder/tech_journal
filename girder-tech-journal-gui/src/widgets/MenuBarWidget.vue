868cfa47271ba72dd8a14cc6a50a159db57b7e26
Brian Helba <brian.helba@kitware.com>
2019-01-31 21:42:54 -0500

    Use safer HTML names for creating Vue components

--- girder-tech-journal-gui/src/widgets/MenuBarWidget.vue
+++ girder-tech-journal-gui/src/widgets/MenuBarWidget.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
-BackboneView#headerBar(:view='MenuBarView', :render='false')
+backbone-view#headerBar(:view='MenuBarView', :render='false')
 </template>
 
 <script>
 import BackboneView from '@/BackboneView.vue';
 import MenuBarView from '@/views/menuBar';
 
-// @vue/component
 export default {
   name: 'MenuBarWidget',
   components: {
