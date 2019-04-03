9df87ec8c7c9ee48fba2e8d70db1b1b3676b5862
Joseph Snyder <joe.snyder@kitware.com>
2019-04-03 16:07:27 -0400

    Ensure "query" is checked before looking for values
    Ensure that there is some length to query before attempting to check
    the value of a set parameter.

--- girder-tech-journal-gui/src/pages/home/home.js
+++ girder-tech-journal-gui/src/pages/home/home.js
@@ -49,11 +49,13 @@ const HomePage = View.extend({
         }
     },
     initialize: function (query) {
-        if (query['query'].indexOf('collection') !== -1) {
-            this.defaultJournal = query['query'].substr(13, query['query'].length - 1);
-            this.collectionID = this.defaultJournal;
-            this.querystring = '*';
-            this.render(this.collectionID, 0);
+        if (query.length > 0) {
+            if (query['query'].indexOf('collection') !== -1) {
+              this.defaultJournal = query['query'].substr(13, query['query'].length - 1);
+              this.collectionID = this.defaultJournal;
+              this.querystring = '*';
+              this.render(this.collectionID, 0);
+            }
         } else {
             restRequest({
                 method: 'GET',
