2c87497432c5ba6f90ce98faf8aee65c075d4433
Joseph Snyder <joe.snyder@kitware.com>
2019-04-30 17:10:16 -0400

    Fix editing of Category lists
    Ensure new categories are saved as categories
    Only show "new object" form for the category that is being manipulated
    Remove "subcategory" event, as the pages is not ready for that
    Consolidate the updating of the categories in the database.

--- girder-tech-journal-gui/src/pages/adminCategories/adminCategories.js
+++ girder-tech-journal-gui/src/pages/adminCategories/adminCategories.js
@@ -17,40 +17,35 @@ const adminCategoriesPage = View.extend({
             });
         },
         'click .AddRootLink': function (event) {
-            $('.AddRootCategory').show();
+            this.$(event.currentTarget.parentNode).find('.AddRootCategory').show();
         },
-        'click .categoryObj': function (event) {
+        /* 'click .categoryObj': function (event) {
             this.categoryObj = event.currentTarget;
             $('.AddCategory').show();
-        },
+        }, */
         'submit #addTree': function (event) {
             event.preventDefault();
             var newCatName = this.$('#newTreeName').val();
             //  Call out to category API to save initial object
             restRequest({
                 method: 'POST',
-                url: `journal/category?text=${newCatName}&tag=disclaimer`
+                url: `journal/category?text=${newCatName}&tag=categories`
             }).done((resp) => {
                 this.$('#treeWrapper').html(this.$('#treeWrapper').html() + adminCategoriesEntryTemplate({'name': newCatName, 'values': []}));
             });
         },
         'submit #addRootCategoryForm': function (event) {
             event.preventDefault();
-            var parent = $(event.currentTarget).parent();
-            parent.siblings('.categoryTree').find('.categoryList').append('<li class="categoryObj">' + $(event.currentTarget).children().first().val() + '</li>');
-            var catName = parent.siblings('h4').text();
-            var valueData = {'key': catName, 'value': []};
-            parent.siblings('.categoryTree').find('.categoryList').find('li').each(function (index, val) {
-                valueData['value'].push($(val).text());
-            });
-
-            restRequest({
-                method: 'PUT',
-                url: 'journal/category?tag=categories',
-                data: {
-                    list: JSON.stringify([valueData])
-                }
-            });
+            var parent = $(event.currentTarget).closest('.TreeEntry');
+            parent.find('.categoryTree').find('.categoryList').append('<div class="categoryEntry"><li class="categoryObj">' +
+                                                                          $(event.currentTarget).children().first().val() +
+                                                                          '</li>&nbsp<a class="removeCategory">Remove</a></div>');
+            this.updateCategory(parent);
+        },
+        'click .removeCategory': function (event) {
+            var parent = $(event.currentTarget).closest('.TreeEntry');
+            $(event.currentTarget).closest('.categoryEntry').remove();
+            this.updateCategory(parent);
         },
         'submit #addNewChildCategory': function (event) {
             event.preventDefault();
@@ -76,6 +71,20 @@ const adminCategoriesPage = View.extend({
             parentView: this
         });
         return this;
+    },
+    updateCategory: function (treeEntry) {
+        var catName = treeEntry.find('h4').text();
+        var valueData = {'key': catName, 'value': []};
+        treeEntry.find('.categoryTree').find('.categoryList').find('li').each(function (index, val) {
+            valueData['value'].push($(val).text());
+        });
+        restRequest({
+            method: 'PUT',
+            url: 'journal/category?tag=categories',
+            data: {
+                list: JSON.stringify([valueData])
+            }
+        });
     }
 });
 
