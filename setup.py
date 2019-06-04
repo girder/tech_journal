a00777ad9c67e1cdc8d26dce76134fce0a0fdce0
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-06-04 14:24:16 -0400

    Add tox.ini and fields in setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 
+with open('README.rst') as readme:
+    long_description = readme.read()
+
 setup(
     name='girder-tech-journal',
     version='0.1.0',
     description='A Girder plugin for a Technical Journal',
+    long_description=long_description,
+    long_description_content_type='text/reStructuredText',
+    url='https://github.com/girder/tech_journal',
+    maintainer='Kitware, Inc.',
+    maintainer_email='kitware@kitware.com',
+    include_package_data=True,
     packages=['girder_tech_journal'],
     install_requires=[
         'girder',
